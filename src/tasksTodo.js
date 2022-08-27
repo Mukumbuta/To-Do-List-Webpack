790ac77876c6a1332130f014f49425a64b2d6deb
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-08-27 08:53:51 -0700

    add tests for interactivity and edit functions

--- src/tasksTodo.js
+++ src/tasksTodo.js
@@ -1,21 +1,41 @@
 import MockStorage from './__mocks__/mockStorage.js';
 
+
 export default class Lists {
   constructor() {
     this.list = [];
   }
-
   addTodo(todo) {
     this.list.push(todo);
     MockStorage.data = this.list;
   }
-
   removeTodo(todoID) {
-    const temp = this.list.filter((todo) => todo.id !== todoID);
-    temp.forEach((todo, index) => {
+    const tasktoRemove = this.list.filter((todo) => todo.id !== todoID);
+    tasktoRemove.forEach((todo, index) => {
       todo.index = index + 1;
     });
-    this.list = temp;
+    this.list = tasktoRemove;
+    MockStorage.data = this.list;
+  }
+  editTodo(todoId, taskDesc) {
+    this.list = this.list.map((todo) => {
+      if (todo.id === Number(todoId)) {
+        return { ...todo, description: taskDesc };
+      }
+      return todo;
+    });
+    MockStorage.data = this.list;
+  }
+  completeTodo(todoId, status) {
+    const checkedTask = this.list.findIndex((element) => element.id === Number(todoId));
+    this.list[checkedTask].completed = status;
+    MockStorage.data = this.list;
+  }
+  clearCompletedTodos() {
+    this.list = this.list.filter((todo) => !todo.completed);
+    this.list.forEach((todo, index) => {
+      todo.index = index + 1;
+    });
     MockStorage.data = this.list;
   }
 }
